36bb9a9eee59d1dd386beaf899601551e4cdbc76
Arthurx-07 <arunrawat8750@gmail.com>
2024-12-31 22:36:30 +0530

    Add files via upload

---
+++ programe.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std ;
+
+int main () {
+    int a ;
+    cin >> a ;
+    cout << a << endl;
+    
+    for ( int i = 0 ; i<= 5 ; i++ ){
+        cout << i << endl;
+    }
+    return 0;
+}
